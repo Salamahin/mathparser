f53174026454a916d87492eec67f879b7c7957b5
salamahin <danilasergeevich@gmail.com>
2016-02-14 15:45:49 +0300

    test passing

--- src/main/java/com.company.home.mathparser/token/TokenizeRuleKeeper.java
+++ src/main/java/com.company.home.mathparser/token/TokenizeRuleKeeper.java
@@ -1,76 +1,66 @@
 package com.company.home.mathparser.token;
 
 import com.company.home.mathparser.token.types.*;
-import com.sun.org.apache.xpath.internal.operations.UnaryOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TokenizeRuleKeeper {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TokenizeRuleKeeper.class);
-
     private boolean duplicatedUnaryOperators(final Token<?> t) {
-        final boolean b = instanceOfClass(UnaryOperation.class, t) &&
+        return instanceOfClass(UnaryOperator.class, t) &&
                 t.getPreviousToken().isPresent() &&
-                instanceOfClass(UnaryOperation.class, t.getPreviousToken().get());
-        if(b)
-            LOG.debug("Token {} is invalid because prev token is unary operation", t);
-
-
-        return b;
+                instanceOfClass(UnaryOperator.class, t.getPreviousToken().get());
     }
 
     private boolean duplicatedValueTokens(final Token<?> t) {
-        final boolean b = instanceOfClass(Value.class, t) &&
+        return instanceOfClass(Value.class, t) &&
                 t.getPreviousToken().isPresent() &&
                 instanceOfClass(Value.class, t.getPreviousToken().get());
-        if(b)
-            LOG.debug("Token {} is invalid because prev token is value", t);
+    }
 
-        return b;
+    private  boolean duplicatedBinaryOperators(final Token<?> t) {
+        return instanceOfClass(BinaryOperator.class, t) &&
+                t.getPreviousToken().isPresent() &&
+                instanceOfClass(BinaryOperator.class, t.getPreviousToken().get());
+    }
+
+    private boolean binaryOperatorAfterUnary(final Token<?> t) {
+        return instanceOfClass(BinaryOperator.class, t) &&
+                t.getPreviousToken().isPresent() &&
+                instanceOfClass(UnaryOperator.class, t.getPreviousToken().get());
     }
 
     private boolean unaryOperationAfterValue(final Token<?> t) {
-        final boolean b = instanceOfClass(UnaryOperator.class, t) &&
+        return instanceOfClass(UnaryOperator.class, t) &&
                 t.getPreviousToken().isPresent() &&
                 instanceOfClass(Value.class, t.getPreviousToken().get());
-
-        if(b)
-            LOG.debug("Token {} is invalid because prev token is value", t);
-
-        return b;
     }
 
     private boolean unaryOperatorAfterRightParenthesis(final Token<?> t) {
-        final boolean b = instanceOfClass(RightParenthesis.class, t) &&
+        return instanceOfClass(UnaryOperator.class, t) &&
                 t.getPreviousToken().isPresent() &&
-                instanceOfClass(UnaryOperator.class, t.getPreviousToken().get());
-
-        if(b)
-            LOG.debug("Token {} is invalid because prev token is right parenthesis", t);
-
-        return b;
+                instanceOfClass(RightParenthesis.class, t.getPreviousToken().get());
     }
 
     private boolean valueAfterRightParenthesis(final Token<?> t) {
-        return instanceOfClass(RightParenthesis.class, t) &&
+        return instanceOfClass(Value.class, t) &&
                 t.getPreviousToken().isPresent() &&
-                instanceOfClass(Value.class, t.getPreviousToken().get());
+                instanceOfClass(RightParenthesis.class, t.getPreviousToken().get());
     }
 
-//    private boolean firstUnaryOperatorInExprOnlyBeforeLeftParenthesis(final Token<?> t) {
-//        return !t.getPreviousToken().isPresent() &&
-//                instanceOfClass(UnaryOperator.class, t);
-//    }
-
     private boolean binaryOperatorIsFirstInExpression(final Token<?> t) {
-        boolean b = instanceOfClass(BinaryOperator.class, t) &&
+        return instanceOfClass(BinaryOperator.class, t) &&
                 !t.getPreviousToken().isPresent();
+    }
 
-        if(b)
-            LOG.debug("Token {} is invalid because there are no prev tokens", t);
+    private boolean binaryOperatorAfterLeftParenthesis(final Token<?> t) {
+        return instanceOfClass(BinaryOperator.class, t) &&
+                t.getPreviousToken().isPresent() &&
+                instanceOfClass(LeftParenthesis.class, t.getPreviousToken().get());
+    }
 
-        return b;
+    private boolean binaryOperatorAfterFuncParamSeparator(final Token<?> t) {
+        return instanceOfClass(BinaryOperator.class, t) &&
+                t.getPreviousToken().isPresent() &&
+                instanceOfClass(FuncParamSep.class, t.getPreviousToken().get());
     }
 
     private <T> boolean instanceOfClass(final Class<?> tClass, final T value) {
@@ -80,9 +70,13 @@ public class TokenizeRuleKeeper {
     public boolean matchesAllRules(final Token<?> token) {
      return !duplicatedUnaryOperators(token) &&
              !duplicatedValueTokens(token) &&
+             !duplicatedBinaryOperators(token) &&
              !unaryOperationAfterValue(token) &&
              !unaryOperatorAfterRightParenthesis(token) &&
              !valueAfterRightParenthesis(token) &&
-             !binaryOperatorIsFirstInExpression(token);
+             !binaryOperatorIsFirstInExpression(token) &&
+             !binaryOperatorAfterLeftParenthesis(token) &&
+             !binaryOperatorAfterUnary(token) &&
+             !binaryOperatorAfterFuncParamSeparator(token);
     }
 }
